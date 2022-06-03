eedd199fbd09f7cbb66ae27f4d690754b16d28f6
solomonlema88@gmail.com <solomonlema88@gmail.com>
2022-06-03 14:00:12 -0700

    here is commit from springboot tool chech 1

--- Main.java
+++ Main.java
@@ -66,7 +66,7 @@ public class Main {
 		System.out.println("Goodbye!");
 	}// end main
 }// end JDBCExample
-=======
+//=======
 public class Main{
 public static void main(String[] args){
 System.out.println("Hello World by HCFCIT");
